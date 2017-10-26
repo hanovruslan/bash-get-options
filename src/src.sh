5bfe950c5c6892df58b3722adcc67a9a5fef04b0
Hanov Ruslan <hanov.ruslan@gmail.com>
2017-10-26 08:11:42 +0300

    getopt refactoring (#7)

--- src/src.sh
+++ src/src.sh
@@ -1,66 +1,69 @@
 #@IgnoreInspection BashAddShebang
-function bgo_export_name () {
-  local option_name="${1}"
-  local default_value="${@:2}"
-
-  [[ -z ${options[${option_name}]} ]] \
-  && export ${option_name}="${default_value}" \
-  || export ${option_name}="$(eval declare -A option=${options[${option_name}]} && echo ${option})"
-}
-function bgo_get_options {
-  local dict=${@: -1};
-  local result;
-  local sep_val=":"
-  local sep_case='|'
-  local opt_pat;
-  local opt_case=();
-  local reg=();
-  local cnt;
-
-  declare -A cnt=()
-  declare -A result=()
-
-  eval "declare -A dict="${dict#*=}
-  for key in ${!dict[@]}
+bgo_get_options () {
+  index=${1}
+  declare -A \
+    dict="$(bgo_get_values ${@:(( 2 + ${index} )):1})" \
+    defaults="$(bgo_get_values ${@:(( 3 + ${index} )):1})" \
+    result
+  values=${@:2:${index}}
+  OPTSTRING=$(bgo_getopt_o ${!options_dict[@]})
+  set -- $(getopt ${OPTSTRING} ${values[@]})
+  shopt -s extglob # to catch opt by template
+  while getopts ${OPTSTRING} key
   do
-    opt_pat=${opt_pat}${key}${sep_val}
-    opt_case+=(${key})
-  done
-  opt_case=$(printf "${sep_case}%s" "${opt_case[@]}")
-  opt_case='@('${opt_case:${#sep_case}}')'
-
-  # `shopt -s extglob` enable var substitution in the case "${opt_case})"
-  shopt -q extglob; extglob_set=$?
-  ((extglob_set)) && shopt -s extglob
-  while getopts ${opt_pat} opt_value
-  do
-    case $opt_value in
-      ${opt_case})
-      optarg=(${OPTARG//${sep_val}/ })
-      var=${dict[${opt_value}]}
-      key=${optarg[0]}
-      value=${optarg[1]:-${key}}
-      if [ -z "${!var}" ]; then
-        reg+=(${var})
-        cnt+=([${var}]=0)
-      fi
-      _value=$( [ "${key}" == "${value}" ] && echo [${cnt[${var}]}]=${value} || echo [${key}]=${value} )
-      cnt[${var}]=$(( ${cnt[${var}]}+1 ));
-      eval "${var}=${!var:-${sep_val}}${_value}${sep_val}"
-      ;; *)
-         echo 'Unknown options'
-         exit 1
-      ;;
+    case "${key}" in
+      $(bgo_get_optcase_template))
+        [ -z "${result[${dict[$key]}]}" ] \
+          && result[${dict[$key]}]="${OPTARG}" \
+          || result[${dict[$key]}]+="${BGO_A_DEL}${OPTARG}"
+        ;;
+      --) shift
+        break;;
+      -*) bgo_fail "Unknown options ${key}"
+        break;;
+      *)  bgo_fail "This is not supposed to be displayed ${key}"
+        break;;
     esac
   done
-  for key in ${reg[@]}
+  for key in "${!defaults[@]}"
   do
-    value=$(echo ${!key}|sed "s/^\\${sep_val}//;s/\\${sep_val}$//")
-    value=(${value//:/ })
-    result[${key}]="(${value[@]})"
+    [ -z "${result[${key}]}" ] && result[${key}]="${defaults[${key}]}"
   done
-  # revert back to previous value
-  ((extglob_set)) && shopt -u extglob
-
   declare -p result
+}
+bgo_get_values () (
+  var=${1}
+  var="$(declare -p ${var})"
+  var="${var#*=}"
+  echo "${var:1:-1}"
+)
+bgo_implode () (
+  local d=$1
+  shift
+  echo -n "$1"
+  shift
+  printf "%s" "${@/#/$d}"
+)
+bgo_get_optcase_template () (
+  echo "+($(bgo_implode ${BGO_CASE_DEL} $( \
+    for key in ${!options_dict[@]}
+    do
+      echo "${key}"
+    done \
+  )))"
+)
+bgo_getopt_o () {
+  for var in "${@}"
+  do
+    echo -n "${var}${BGO_OPT_DEL}"
+  done
+}
+bgo_explode () {
+  echo ${1//${2}/${BGO_A_DEL}}
+}
+bgo_fail () {
+  echo ${1} >&2; exit 1
+}
+bgo_export () {
+  bgo_fail x
 }
